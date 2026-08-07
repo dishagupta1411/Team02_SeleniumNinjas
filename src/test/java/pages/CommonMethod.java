dda966a9d53b61ba3201e6c5412b28543c836b98
Neha Saini <nehasaini.mca09@gmail.com>
2026-08-07 16:49:44 -0700

    Commit

--- src/test/java/pages/CommonMethod.java
+++ src/test/java/pages/CommonMethod.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -17,43 +18,66 @@ import utilities.ConfigReader;
 
 public class CommonMethod {
 
-	protected WebDriver driver;
-	protected Actions actions;
-	protected JavascriptExecutor js;
-	protected WebDriverWait wait;
-	protected ConfigReader config;
+    protected WebDriver driver;
+    protected Actions actions;
+    protected JavascriptExecutor js;
+    protected WebDriverWait wait;
+    protected ConfigReader config;
 
-	public CommonMethod(Passing_Driver passdr) throws IOException {
+    public CommonMethod(Passing_Driver passdr) throws IOException {
 
-	    this.driver = passdr.getDriver();
+        this.driver = passdr.getDriver();
 
-	    if(driver == null) {
-	        throw new IllegalStateException("WebDriver is null. Driver was not initialized before Page Object creation.");
-	    }
+        if (driver == null) {
+            throw new IllegalStateException(
+                "WebDriver is null. Driver was not initialized."
+            );
+        }
 
-	    this.actions = new Actions(driver);
-	    this.js = (JavascriptExecutor) driver;
-	    this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	    this.config = new ConfigReader();
-	    PageFactory.initElements(driver, this);
-	}
+        this.actions = new Actions(driver);
+        this.js = (JavascriptExecutor) driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.config = new ConfigReader();
 
-	
+        PageFactory.initElements(driver, this);
+    }
 
-	protected void safeClick(By locator) {
-		wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
-	}
+    public void navigateToApplication() {
+        driver.get(config.getProperty("baseUrl"));
+    }
+    
+    protected void safeClick(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
 
-	protected void safeClick(WebElement element) {
-		wait.until(ExpectedConditions.elementToBeClickable(element)).click();
-	}
+    protected void safeClick(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
 
-	
-	protected void safeType(WebElement element, String text) {
-		wait.until(ExpectedConditions.visibilityOf(element));
-		element.sendKeys(text);
-	}
+    protected void safeType(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
 
-	
+    protected String getText(WebElement element) {
+        return wait.until(
+            ExpectedConditions.visibilityOf(element)
+        ).getText().trim();
+    }
+
+    protected boolean isDisplayed(WebElement element) {
+        return wait.until(
+            ExpectedConditions.visibilityOf(element)
+        ).isDisplayed();
+    }
+
+	protected boolean waitForUrlContains(String partialUrl) {
+        try {
+            return wait.until(ExpectedConditions.urlContains(partialUrl));
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
 	
 }
