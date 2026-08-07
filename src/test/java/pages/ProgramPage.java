71361e6142c09a4537c1fe4df61dac0cbc670263
Neha Saini <nehasaini.mca09@gmail.com>
2026-08-07 16:39:36 -0700

    Page and StepDef

--- src/test/java/pages/ProgramPage.java
+++ src/test/java/pages/ProgramPage.java
@@ -1,7 +1,10 @@
 package pages;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -24,7 +27,24 @@ public class ProgramPage extends CommonMethod {
 	private WebElement admin;
 	
 	@FindBy(xpath="//div[text()=' Dashboard']")
-	WebElement dashboardHeader;
+	private WebElement dashboardHeader;
+	@FindBy(xpath="//span[text()='Program']")
+	private WebElement programBtn;
+	@FindBy(xpath="//button[text()='Add New Program']")
+	private WebElement addNewProgram;
+	@FindBy(xpath="//div[text()=' Manage Program']")
+	private WebElement manageProgram;
+	@FindBy(xpath = "//mat-card")
+	private WebElement programPanel;
+	@FindBy(xpath = "//button[@icon='pi pi-trash']")
+	private WebElement multipleDeletetn;
+	@FindBy(xpath="//input[@placeholder='Search...']")
+	private WebElement searchBox;
+	@FindBy(xpath=("//*[@class='p-datatable-thead']/tr"))
+	private WebElement manageprogramTable;
+	
+	
+	
 	
 		public ProgramPage(Passing_Driver driver) throws IOException {
 		super(driver); 
@@ -35,8 +55,7 @@ public class ProgramPage extends CommonMethod {
 	
 		public void addUserNameAndPassword() {
 			safeType(userName, "Lmshackathon@gmail.com");
-			safeType(password, "lmsAug@2026");
-			
+			safeType(password, "lmsAug@2026");			
 		}
 		
 		public void selectRole() {
@@ -50,5 +69,63 @@ public class ProgramPage extends CommonMethod {
 		public Boolean isDashboardDisplayed() {
 			return dashboardHeader.isDisplayed();
 		}
+		
+		public void clickOnProgramBtn() {
+			safeClick(programBtn);
+		}
+		
+		public boolean isOnProgramPage() {
+			return waitForUrlContains("program");
+		}
+		
+		public String isAddNewProgramDisplayed() {
+			return getText(addNewProgram);
+		}
+		
+		public String isManageProgramDisplayed() {
+			return getText(manageProgram);
+			
+		}
+		
+		public int getManageProgramHeading() {
+			return manageProgram.getLocation().getX();
+		}
+		
+		public int getProgramPanel() {
+			return programPanel.getLocation().getX();
+		}
+		
+		
+		public int getDeleteBtnPx() {
+			return multipleDeletetn.getLocation().getX();
+		}
+		
+		public boolean isMultipleDeleteButtonDisabled() {
+			return multipleDeletetn.isEnabled();
+		}
+		
+		public boolean isSearchBarDisplayed() {
+			return searchBox.isDisplayed();
+		}
+		
+		public String getSearchPlaceholder() {
+		    return searchBox.getAttribute("placeholder");
+		}
+		
+		public List<String> testManageProgramMenu() {
+
+		//	List<WebElement> manageProgramMenuItems = manageProgram.findElements(By.tagName("th"));
+			 List<WebElement> manageProgramMenuItems = driver.findElements(
+				        By.xpath("//tr[th[contains(normalize-space(), 'Program Name')]]/th[position() > 1]")
+				    );
+		    List<String> itemTextList = new ArrayList<>();
+
+		    for (WebElement item : manageProgramMenuItems) {
+		        itemTextList.add(item.getText().trim());
+		    }
+
+		    return itemTextList;
+		}
+		
 		
 }
--- src/test/java/stepDefinitions/ProgramStepDef.java
+++ src/test/java/stepDefinitions/ProgramStepDef.java
@@ -1,6 +1,8 @@
 package stepDefinitions;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.testng.Assert;
 
@@ -39,19 +41,63 @@ public class ProgramStepDef {
 	}
 	   
 	
-	@When("Admin clicks {string} on the navigation bar")
-	public void admin_clicks_on_the_navigation_bar(String string) {
+	@When("Admin clicks Program on the navigation bar")
+	public void admin_clicks_on_the_navigation_bar() {
+		program.clickOnProgramBtn();
 	    
 	}
 	
 	@Then("Admin should be navigated to Program page")
 	public void admin_should_be_navigated_to_program_page() {
+		Assert.assertTrue(program.isOnProgramPage(), "Admin is not on Program Page");
 	    
 	}
-	@Then("Admin should see the Edit and Delete buttons on each row of the data table")
-	public void admin_should_see_the_edit_and_delete_buttons_on_each_row_of_the_data_table() {
+	
+	@Then("Admin should see sub menu in menu bar as {string}")
+	public void admin_should_see_sub_menu_in_menu_bar_as(String string) {
+	    Assert.assertEquals(program.isAddNewProgramDisplayed(),string,"Add new Program is not there");
+	}
+	
+	@Then("Admin should see the heading {string}")
+	public void admin_should_see_the_heading(String string) {
+		 Assert.assertEquals(program.isManageProgramDisplayed(),string,"Admin is not on manage program page");
+	   
+	}
+	
+	@Then("Admin should see manage program heading aligned on the left side")
+	public void admin_should_see_manage_program_heading_aligned_on_the_left_side() {
+		  Assert.assertEquals(program.getManageProgramHeading()-16,program.getProgramPanel(), "Manage Program heading is not left aligned.");
 	    
 	}
+	
+	@Then("Admin should see a Delete button in left top is disabled")
+	public void admin_should_see_a_delete_button_in_left_top_is_disabled() {
+	    Assert.assertTrue(!(program.isMultipleDeleteButtonDisabled()), "Multiple delete button is not disabled");
+	    Assert.assertEquals(program.getDeleteBtnPx(),program.getProgramPanel()+16, "Manage Program heading is not left aligned.");
+	    
+	}
+	
+	@Then("Admin should see Search bar")
+	public void admin_should_see_search_bar() {
+	    Assert.assertTrue(program.isSearchBarDisplayed(),"Search bar is not displayed");
+	}
+	
+	@Then("Admin should see {string} placeholder text")
+	public void admin_should_see_Search_placeholder_text(String text) {
+		Assert.assertEquals(program.getSearchPlaceholder(),text,"Search placeholder text doesn't match");
+		
+	}
+	
+	@Then("Admin should see data table with column header on the Manage Program Page as Program Name, Program Description, Program Status, Edit\\/Delete")
+public void admin_should_see_data_table_with_column_header_on_the_manage_program_page_as_program_name_program_description_program_status_edit_delete() {
+		 List<String> actualHeaders = program.testManageProgramMenu();
+
+		  List<String> expectedHeaders = Arrays.asList("Program Name ","Program Description ","Program Status "," Edit / Delete ");
+
+		    Assert.assertEquals(actualHeaders, expectedHeaders);
+		}
+			
+	
 }
 
 
