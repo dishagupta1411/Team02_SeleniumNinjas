b8a45eb73245fd33bfa186681b387d92b8a5988d
Neha Saini <nehasaini.mca09@gmail.com>
2026-08-08 22:56:33 -0700

    AddNewProgram,Edit Program and Pagination

--- src/test/java/pages/ProgramPage.java
+++ src/test/java/pages/ProgramPage.java
@@ -15,6 +15,8 @@ import driver.Passing_Driver;
 public class ProgramPage extends CommonMethod {
 	
 	
+	
+	 List<WebElement> manageProgramMenuItems=new ArrayList<>();
 	@FindBy(id = "username")
 	private WebElement userName;
 	@FindBy(id = "password")
@@ -42,7 +44,94 @@ public class ProgramPage extends CommonMethod {
 	private WebElement searchBox;
 	@FindBy(xpath=("//*[@class='p-datatable-thead']/tr"))
 	private WebElement manageprogramTable;
+	@FindBy(xpath="//div[@class='p-checkbox p-component']")
+	private WebElement checkBoxWithManageProgram;
+	@FindBy(css = "p-tablecheckbox")
+	private List<WebElement> tableCheckbox;
+	@FindBy(css = "p-sorticon")
+	private List<WebElement> tableSorticon;
+	@FindBy(css = ".p-datatable-tbody")
+	private List<WebElement> tablebody;
+	@FindBy(css = "#editProgram")
+	private List<WebElement> editPrograms;
+	@FindBy(css = "#deleteProgram")
+	private List<WebElement> deletePrograms;
+	@FindBy(xpath= "//p-table/div/div[2]/div")
+	private WebElement paginationfooter;
+	@FindBy(xpath = "//p-table/div/p-paginator/div/span[1]")
+	private WebElement paginationInfoUI;
+	@FindBy(xpath = "//p-table/div/p-paginator/div/button[1]/span")
+	private WebElement doubleLeftIcon;
+	@FindBy(xpath = "//p-table/div/p-paginator/div/button[4]/span")
+	private WebElement doubleRightIcon;
+	@FindBy(xpath = "//p-table/div/p-paginator/div/button[2]")
+	private WebElement leftIcon;
+	@FindBy(xpath = "//p-table/div/p-paginator/div/button[3]")
+	private WebElement rightIcon;
 	
+	//--------------------Add New Program----------------------------------------
+	
+	@FindBy(xpath = "//app-program/p-dialog/div/div")
+	private WebElement popUp;
+	@FindBy(xpath = "//span[text()='Cancel']")
+	private WebElement cancelButton;
+	@FindBy(xpath = "//span[text()='Program Details']")
+	private WebElement titlePopUp;
+	@FindBy(xpath = "//label[text()='Name']")
+	private WebElement programName;
+	@FindBy(xpath = "//span[text()='*']")
+	private WebElement astreik;
+	@FindBy(id = "saveProgram")
+	private WebElement saveProgram;
+	@FindBy(xpath = "//app-program/p-dialog/div/div/div[2]/div[1]/small")
+	private WebElement requiredText;
+	@FindBy(id = "programName")
+	private WebElement pname;
+	@FindBy(xpath = "//Label[@for='programDescription']")
+	private WebElement pDescription;
+	@FindBy(xpath = "//p-radiobutton[@id='category']//input[@value='Active']")
+	private WebElement active;
+	@FindBy(xpath = "//p-radiobutton[@id='category']//input[@value='Inactive']")
+	private WebElement inActive;
+	@FindBy(xpath = "//*[@id='saveProgram']/span[2]")
+	private WebElement save;
+	@FindBy(xpath = "//app-program/p-toast/div/p-toastitem/div/div/div/div[2]")
+	private WebElement successMsg;
+	@FindBy(xpath = "//div[text()='Program Created Successfully']")
+	private WebElement enteredPName;
+	@FindBy(css = "button.p-dialog-header-close")
+	private WebElement xButton;
+	@FindBy(xpath="//small[text()='Program name is required.']")
+	private WebElement pNameRequired;
+	@FindBy(xpath="//small[text()='Status is required.']")
+	private WebElement pStatusRequired;
+	@FindBy(xpath="//input[@id='programName']")
+	private WebElement programNameTextBox;
+	@FindBy(xpath="//input[@id='programDescription']")
+	private WebElement programDescTextBox;
+	@FindBy(xpath="//div[@class='p-radiobutton-box']")
+	private WebElement activeradiobutton;
+	@FindBy(xpath="//app-program/div/mat-card/mat-card-content/p-table/div/div[1]/table/tbody/tr[1]/td[2]")
+	private WebElement enteredProgramName;
+	@FindBy(xpath="//input[@id='programDescription']")
+	private WebElement enteredDescription;
+	@FindBy(xpath= "//button[@id='editProgram']")
+	private WebElement editButton;
+	
+	
+	// Pagination frame locators
+	@FindBy(xpath = "//span[contains(@class, 'p-paginator-current')]")
+	private WebElement currentEntriesText;
+	@FindBy(xpath = "//button[contains(@class, 'p-paginator-first')]")
+	private WebElement firstButton;
+	@FindBy(xpath = "//button[contains(@class, 'p-paginator-prev')]")
+	private WebElement prevButton;
+	@FindBy(xpath = "//button[contains(@class, 'p-paginator-next')]")
+	private WebElement nextButton;
+	@FindBy(xpath = "//button[contains(@class, 'p-paginator-last')]")
+	private WebElement lastButton;
+
+
 	
 	
 	
@@ -113,13 +202,10 @@ public class ProgramPage extends CommonMethod {
 		}
 		
 		public List<String> testManageProgramMenu() {
+		    List<WebElement> manageProgramMenuItems = driver.findElements(
+		        By.xpath("//tr[th[contains(normalize-space(), 'Program Name')]]/th[position() > 1]"));
 
-		//	List<WebElement> manageProgramMenuItems = manageProgram.findElements(By.tagName("th"));
-			 List<WebElement> manageProgramMenuItems = driver.findElements(
-				        By.xpath("//tr[th[contains(normalize-space(), 'Program Name')]]/th[position() > 1]")
-				    );
 		    List<String> itemTextList = new ArrayList<>();
-
 		    for (WebElement item : manageProgramMenuItems) {
 		        itemTextList.add(item.getText().trim());
 		    }
@@ -127,5 +213,276 @@ public class ProgramPage extends CommonMethod {
 		    return itemTextList;
 		}
 		
+		public boolean ischeckBoxDisplayed() {
+			return !(searchBox.isSelected());
+		}
 		
+		public boolean testAreUnchecked() {
+		    for (WebElement checkbox : tableCheckbox) {
+		        if (checkbox.isSelected()) {
+		            return false;
+		        }
+		    }
+		    return true;
+		}
+		
+		public boolean testSortArrowsVisibility() {
+		    for (WebElement item : manageProgramMenuItems) {
+		        String itemText = item.getText().trim();
+		        boolean hasSortArrow = false;
+
+		        for (WebElement sortIcon : tableSorticon) {
+		            if (item.findElements(By.xpath(".//p-sorticon")).contains(sortIcon)) {
+		                hasSortArrow = true;
+		                break;
+		            }}
+		          if ("Edit / Delete".equals(itemText) || itemText.isEmpty()) {
+		            if (hasSortArrow) {
+		                return false;
+		            }
+		        } else {
+		            if (!hasSortArrow) {
+		                return false;
+		            }     }	    }
+		    return true;
+		}
+		
+		public boolean testEditDeleteButtonsPresence() {
+
+		    for (WebElement editButton : editPrograms) {
+		        if (!editButton.isDisplayed()) {
+		            return false;
+		        }
+		    }
+		    for (WebElement deleteButton : deletePrograms) {
+		        if (!deleteButton.isDisplayed()) {
+		            return false;
+		        }	    }
+		    return true;
+		}
+
+		public boolean testPaginationTextIcons() {
+
+		    String paginationText = paginationInfoUI.getText().trim();
+		    String expectedPattern = "Showing \\d+ to \\d+ of \\d+ entries";
+
+		    if (!paginationText.matches(expectedPattern)) {
+		        return false;
+		    }
+		    if (!doubleLeftIcon.isDisplayed()) {
+		        return false;
+		    }
+		    if (!doubleRightIcon.isDisplayed()) {
+		        return false;
+		    }
+		    if (!leftIcon.isDisplayed()) {
+		        return false;
+		    }
+		    if (!rightIcon.isDisplayed()) {
+		        return false;
+		    }
+		    return true;
+		}
+		
+		public boolean testPaginationFooter() {
+
+		    String paginationTextFooter = paginationfooter.getText().trim();
+		    String expectedPatternFooter = "In total there are \\d+ programs.";
+		    return paginationTextFooter.matches(expectedPatternFooter);
+		}
+		
+		
+		
+	
+		
+		
+		public void clickAddNewProgram() {
+		    safeClick(addNewProgram); ;
+		}
+
+		public boolean isProgramDetailsDialogDisplayed() {
+		    return isDisplayed(popUp);
+		}
+
+		public String getProgramDetailsTitle() {
+		    return titlePopUp.getText().trim();
+		}
+
+		public boolean isMandatoryFieldAsteriskDisplayed() {
+		    return isDisplayed(astreik);
+		}
+
+		public String getRequiredFieldMessage() {
+		    return requiredText.getText().trim();
+		}
+
+		public boolean isNameTextBoxDisplayed() {
+		    return isDisplayed(pname);
+		}
+
+		public boolean isDescriptionTextBoxDisplayed() {
+		    return isDisplayed(pDescription);
+		}
+
+		public boolean areStatusRadioButtonsDisplayed() {
+		    return isDisplayed(active) && isDisplayed(inActive);
+		}
+
+		public void clickCancelButton() {
+		    		    safeClick(cancelButton);
+		}
+
+		public void clickSaveProgramButton() {
+		    		    safeClick(saveProgram);
+		}
+
+		public void enterProgramName(String name) {
+		    pname.sendKeys(name);
+		}
+
+		public String getEnteredProgramName() {
+		    return pname.getAttribute("ng-reflect-model");
+		}
+
+		public void enterDescription(String description) {
+		    pDescription.sendKeys(description);
+		}
+
+		public String getEnteredDescription() {
+		    return pDescription.getAttribute("ng-reflect-model");
+		}
+
+		public void clickActiveStatus() {
+		        safeClick(active);
+		}
+
+		public boolean isActiveStatusSelected() {
+		    return active.isSelected();
+		}
+
+		public void clickSaveButton() {
+		        safeClick(saveProgram);
+		}
+
+		public String getSuccessMessage() {
+		    return successMsg.getText().trim();
+		}
+
+		public void clickSearch() {
+		        safeClick(searchBox);
+		}
+
+		public void enterSearchText(String programName) {
+		    searchBox.sendKeys(programName);
+		}
+
+		public String getSearchResultProgramName() {
+		    return enteredPName.getText().trim();
+		}
+
+		public void clickXButton() {
+		       safeClick(xButton);
+		}
+
+		public boolean isProgramDetailsDialogClosed() {
+		    return !isDisplayed(popUp);
+		}
+		
+		public String getRequiredProgramNameMsg() {
+			return getText(pNameRequired);		
+			
+		}
+		
+		public String getRequiredProgramStatusMsg() {
+			return getText(pStatusRequired);		
+			
+		}
+		
+		public void mandotaryFieldName() {
+			programNameTextBox.sendKeys("CypressLMSApplication");	
+			
+		}
+		
+        public void FieldDescription() {
+			programDescTextBox.sendKeys("LMS application");
+		}
+
+        public void mandotaryFieldStatus() {
+        	safeClick(activeradiobutton);
+        	
+        	}
+        
+        public void enterProgramNameInSearchBox(String programName) {
+        	   getText(enteredProgramName);
+        	   
+        }
+
+        public String searchForProgram() {  	   
+        	searchBox.sendKeys("SitaLmsapp");
+        	String searchText =	getText(enteredProgramName);
+	    	return searchText;
+	    		    	
+	    }
+        
+        public String searchForProgramDescription() {  	   
+        	searchBox.sendKeys("SitaLmsapp");
+        	String searchText =	getText(enteredDescription);
+	    	return searchText;
+	    		    	
+	    }
+		
+        public void clickOnEditButton() {
+            clickOnButton(editButton);
+        }
+
+        public void editProgramName() {
+            programNameTextBox.clear();
+            programNameTextBox.sendKeys("Javasaase");
+        }
+		
+        public void editProgramDescription() {
+            programDescTextBox.clear();
+            programDescTextBox.sendKeys("Javas app");
+        }
+		
+        public String searchForUpdatedProgram() {  	   
+        	searchBox.sendKeys("Javasaa");
+        	String searchText =	getText(enteredProgramName);
+	    	return searchText;
+	    		    	
+	    }
+        
+   //---------------------------------------------------------------------------------------------     
+        public boolean isNextButtonEnabled() {
+            return !nextButton.getAttribute("class").contains("p-disabled");
+        }
+
+        public boolean isPrevButtonEnabled() {
+            return !prevButton.getAttribute("class").contains("p-disabled");
+        }
+
+        public void clickNextPage() {
+            if (isNextButtonEnabled()) {
+           clickOnButton(nextButton);
+            }
+        }
+
+        public void clickPreviousPage() {
+            if (isPrevButtonEnabled()) {
+            	clickOnButton(prevButton);
+            }
+        }
+
+        public void clickFirstPage() {
+           
+            clickOnButton(firstButton);
+        }
+
+        public void clickLastPage() {            
+            clickOnButton(lastButton);
+        }
+
+        public String getCurrentEntriesText() {
+            return currentEntriesText.getText().trim();
+}
 }
--- src/test/java/stepDefinitions/ProgramStepDef.java
+++ src/test/java/stepDefinitions/ProgramStepDef.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import driver.Passing_Driver;
 import io.cucumber.java.en.Given;
@@ -15,6 +16,7 @@ import pages.ProgramPage;
 public class ProgramStepDef {
 	
 	private final ProgramPage program;
+	public SoftAssert softAssert = new SoftAssert();
 
 	public ProgramStepDef(Passing_Driver passdr) throws IOException {
 		this.program = new ProgramPage(passdr);
@@ -66,25 +68,25 @@ public class ProgramStepDef {
 	
 	@Then("Admin should see manage program heading aligned on the left side")
 	public void admin_should_see_manage_program_heading_aligned_on_the_left_side() {
-		  Assert.assertEquals(program.getManageProgramHeading()-16,program.getProgramPanel(), "Manage Program heading is not left aligned.");
+		softAssert.assertEquals(program.getManageProgramHeading()-16,program.getProgramPanel(), "Manage Program heading is not left aligned.");
 	    
 	}
 	
 	@Then("Admin should see a Delete button in left top is disabled")
 	public void admin_should_see_a_delete_button_in_left_top_is_disabled() {
-	    Assert.assertTrue(!(program.isMultipleDeleteButtonDisabled()), "Multiple delete button is not disabled");
-	    Assert.assertEquals(program.getDeleteBtnPx(),program.getProgramPanel()+16, "Manage Program heading is not left aligned.");
+	    softAssert.assertTrue(!(program.isMultipleDeleteButtonDisabled()), "Multiple delete button is not disabled");
+	    softAssert.assertEquals(program.getDeleteBtnPx(),program.getProgramPanel()+16, "Manage Program heading is not left aligned.");
 	    
 	}
 	
 	@Then("Admin should see Search bar")
 	public void admin_should_see_search_bar() {
-	    Assert.assertTrue(program.isSearchBarDisplayed(),"Search bar is not displayed");
+	    softAssert.assertTrue(program.isSearchBarDisplayed(),"Search bar is not displayed");
 	}
 	
 	@Then("Admin should see {string} placeholder text")
 	public void admin_should_see_Search_placeholder_text(String text) {
-		Assert.assertEquals(program.getSearchPlaceholder(),text,"Search placeholder text doesn't match");
+		softAssert.assertEquals(program.getSearchPlaceholder(),text,"Search placeholder text doesn't match");
 		
 	}
 	
@@ -92,12 +94,314 @@ public class ProgramStepDef {
 public void admin_should_see_data_table_with_column_header_on_the_manage_program_page_as_program_name_program_description_program_status_edit_delete() {
 		 List<String> actualHeaders = program.testManageProgramMenu();
 
-		  List<String> expectedHeaders = Arrays.asList("Program Name ","Program Description ","Program Status "," Edit / Delete ");
-
+		 List<String> expectedHeaders = Arrays.asList("Program Name","Program Description","Program Status","Edit / Delete");
 		    Assert.assertEquals(actualHeaders, expectedHeaders);
 		}
 			
+	@Then("Admin should see checkbox default state as unchecked beside Program Name column header")
+	public void admin_should_see_checkbox_default_state_as_unchecked_beside_program_name_column_header() {
+		 Assert.assertTrue(program.ischeckBoxDisplayed(),"Checkbox is not unchecked");
+	    
+	}
 	
+	@Then("Admin should see check box default state as unchecked on the left side in all rows against program name")
+	public void admin_should_see_check_box_default_state_as_unchecked_on_the_left_side_in_all_rows_against_program_name() {
+		Assert.assertTrue(program.testAreUnchecked(),"Checkbox is checked when it should be unchecked"
+		    );
+	   
+	}
+	
+	@Then("Admin should see the sort arrow icon beside to each column header except Edit and Delete")
+	public void admin_should_see_the_sort_arrow_icon_beside_to_each_column_header_except_edit_and_delete() {
+		softAssert.assertTrue(program.testSortArrowsVisibility(),"Sort arrow visibility is not as expected");
+	    
+	}
+	
+	@Then("Admin should see the Edit and Delete buttons on each row of the data table")
+	public void admin_should_see_the_edit_and_delete_buttons_on_each_row_of_the_data_table() {
+		 softAssert.assertTrue(program.testEditDeleteButtonsPresence(),"Edit and Delete buttons should be visible in every row");
+	    
+	}
+	
+	@Then("Admin should see the text as Showing x to y of z entries along with Pagination icon below the table.")
+	public void admin_should_see_the_text_as_along_with_Pagination_icon_below_the_table() {
+		
+		program.testPaginationTextIcons();
+	}
+
+	@Then("Admin should see the footer as {string}.")
+	public void admin_should_see_the_footer_as(String string) {
+		
+		program.testPaginationFooter();
+		
+	    	}
+	
+	//----------------------------------------Add New Program UI-------------------------------------------------------
+	
+	@Given("Admin is on Program page")
+	public void admin_is_on_program_page() {
+		program.clickOnProgramBtn();	    
+	}
+	
+	@When("Admin clicks on {string} under the {string} menu bar")
+	public void admin_clicks_on_under_the_menu_bar(String string, String string2) {
+		program.clickAddNewProgram();
+	}
+
+	@Then("Admin should see Program Details dialog")
+	public void admin_should_see_program_details_dialog() {
+		Assert.assertTrue(program.isProgramDetailsDialogDisplayed(),"Program Details dialog should be displayed");
+	    
+	}
+	
+	@Then("Admin should see title as {string}")
+	public void admin_should_see_title_as(String string) {
+		 softAssert.assertEquals(program.getProgramDetailsTitle(),string,"Program Details title is incorrect");
+	    
+	}
+
+	@Then("Admin should see red asterisk mark beside mandatory field {string} and {string}")
+	public void admin_should_see_red_asterisk_mark_beside_mandatory_field_and(String string, String string2) {
+	    
+	}
+
+	@Then("Admin should see the Name text box")
+	public void admin_should_see_the_name_text_box() {
+		   Assert.assertTrue(program.isNameTextBoxDisplayed(),"Program Name text box should be displayed");
+	    
+	}
+
+	@Then("Admin should see the Description text box")
+	public void admin_should_see_the_description_text_box() {
+		 Assert.assertTrue(program.isDescriptionTextBoxDisplayed(),"Program Description text box should be displayed");
+	    
+	}
+
+	@Then("Admin should see Active and Inactive radio buttons")
+	public void admin_should_see_active_and_inactive_radio_buttons() {
+		//Assert.assertTrue(program.areStatusRadioButtonsDisplayed(),"Active and Inactive radio buttons should be displayed");
+	    
+	}
+
+	@Given("Admin is on Program details dialog box")
+	public void admin_is_on_program_details_dialog_box() {
+		program.clickOnProgramBtn();
+		 program.clickAddNewProgram();
+		 Assert.assertTrue(program.isProgramDetailsDialogDisplayed(),"Admin should be on Program Details dialog");
+	    
+	}
+	
+	@When("Admin clicks save button without entering mandatory")
+	public void admin_clicks_save_button_without_entering_mandatory() {
+		program.clickSaveButton();
+	}
+	
+	
+	
+	@Then("Admin gets message {string}")
+	public void admin_gets_message(String expectedMessage) {
+		String actualMessage;
+
+
+	    if (expectedMessage.equals("Program name is required.")) {
+	        actualMessage = program.getRequiredProgramNameMsg();
+	    } else if (expectedMessage.equals("Status is required.")) {
+	        actualMessage = program.getRequiredProgramStatusMsg();
+	    } else {
+	        throw new IllegalArgumentException("Unknown expected message: " + expectedMessage);
+	    }
+	    Assert.assertEquals(actualMessage, expectedMessage, "Message not found");
+	
+	}
+
+	
+	@When("Admin clicks Cancel button")
+	public void admin_clicks_cancel_button() {
+		program.clickCancelButton();
+	    
+	}
+
+	@Then("Admin can see Program Details form disappears")
+	public void admin_can_see_program_details_form_disappears() {
+		Assert.assertTrue(program.isOnProgramPage(), "Admin is not on Program Page");
+		
+	    
+	}
+
+	@When("Admin clicks X button")
+	public void admin_clicks_x_button() {
+		
+		program.clickXButton();
+	    
+	}
+	
+	@When("Admin enter valid details for mandatory fields and Click on save button")
+	public void admin_enter_valid_details_for_mandatory_fields_and_click_on_save_button()  {
+	    program.mandotaryFieldName();
+	    program.FieldDescription();
+	    program.mandotaryFieldStatus();
+	    program.clickSaveButton();
+	}
+	
+	 
+
+	 @Then("Admin gets success message {string}")
+		public void admin_gets_success_message(String field) throws InterruptedException {
+			softAssert.assertEquals(program.getSuccessMessage(),field,"Message not found");
+			    
+		}
+	 
+		@When("Admin searches with newly created {string}")
+		public void admin_searches_with_newly_created(String string) {
+		   program.searchForProgram();
+		}
+
+		@Then("Admin should see the Records of the newly created Program details")
+		public void admin_should_see_the_records_of_the_newly_created_program_details() {
+			String searchText=program.searchForProgram();
+			String expected="SitaLmsApp";
+			softAssert.assertEquals(expected,searchText);
+			softAssert.assertAll();
+		}
+	
+	@Then("Admin gets message {string} is required")
+	public void admin_gets_message_is_required(String field) {
+		//softAssert.assertEquals(program.getSuccessMessage(),field,"Message not found");
+	    
+	}
+	
+	@When("Admin enters a numeric value as the Program Name")
+	public void admin_enters_a_numeric_value_as_the_program_name() {
+	    
+	}
+
+	@Then("Admin should see error message {string}")
+	public void admin_should_see_error_message(String string) {
+	    
+	}
+	
+	
+	@When("Admin clicks on Edit option for particular program")
+	public void admin_clicks_on_edit_option_for_particular_program() {
+		program.clickOnEditButton();
+	   
+	}
+	
+	@When("Admin clicks save button after editing the program name")
+	public void admin_clicks_save_button_after_editing_the_program_name() {
+		program.searchForProgram();
+		program.clickOnEditButton();
+		program.editProgramName();
+		program.clickSaveProgramButton();
+	  
+	}
+	
+	@When("Admin clicks save button after editing the description")
+	public void admin_clicks_save_button_after_editing_the_description() {
+		program.searchForProgramDescription();
+		program.clickOnEditButton();
+		program.editProgramName();
+		program.clickSaveProgramButton();
+	    
+	}
+	
+	
+	@When("Admin searches with newly updated {string}")
+	public void admin_searches_with_newly_updated(String string) {
+		program.searchForUpdatedProgram();
+	   
+	}
+
+	@Then("Admin verifies that the details are correctly updated.")
+	public void admin_verifies_that_the_details_are_correctly_updated() {
+		String searchText=program.searchForProgram();
+		String expected="Javasaa";
+		softAssert.assertEquals(expected,searchText);
+		softAssert.assertAll();
+	}
+	   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Then("Admin should see {string} message")
+	public void admin_should_see_message(String string) {
+	    
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Pagination
+	@When("^Admin clicks the (Next|Last|Previous|First) link on the data table in program page$")
+	public void adminClicksPageLink_program(String pageLink) throws InterruptedException {
+
+		program.clickOnProgramBtn();	
+
+	    switch (pageLink.toLowerCase()) {
+	        case "next":
+	            program.clickNextPage();
+	            break;
+	        case "last":
+	            program.clickLastPage();
+	            break;
+	        case "previous":
+	            program.clickPreviousPage();
+	            break;
+	        case "first":
+	            program.clickFirstPage();
+	            break;
+	        default:
+	            Assert.fail("Unexpected page link: " + pageLink);
+	    }
+	}
+			@Then("^Admin should see the (.*) on the data table in program module$")
+			public void adminShouldSeeResults_program(String expectedResult) {
+
+				switch (expectedResult.toLowerCase().trim()) {
+
+			    case "next enabled link":
+			        Assert.assertTrue(program.isNextButtonEnabled(),"Expected Next button to be enabled.");
+			        break;
+			    case "next button disabled":
+			        Assert.assertFalse(program.isNextButtonEnabled(),"Expected Next button to be disabled.");
+			        break;
+			    case "first page reached":
+			        Assert.assertFalse(program.isPrevButtonEnabled(),"Expected Previous button to be disabled on first page.");
+			        break;
+			    case "previous button disabled":
+			        Assert.assertFalse(program.isPrevButtonEnabled(),"Expected Previous button to be disabled.");
+			        break;
+			    case "next page reached":
+			    	Assert.assertTrue(program.isPrevButtonEnabled(),"Expected Previous button to be enabled after moving to next page.");
+			        break;
+			    default:
+			        Assert.fail("Unexpected result description: " + expectedResult);
+			}}
 }
 
 
